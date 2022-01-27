d71045e604920cc9cefd8a4547d6858a77b5e1ec
Nayan Jain <nayanjain1994@gmail.com>
2022-01-27 13:27:06 -0500

    Header

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+// import MyDropzone from './Upload'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <MyDropzone /> */}
+    </div>
+  );
+}
+
+export default App;
+
---
+++ app/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Header.css"
+const Header = () =>  {
+    return (
+        <nav>
+            <h1>Photo App</h1>
+        </nav>
+    )
+}
+
+export default Header;
